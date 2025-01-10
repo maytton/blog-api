ba1ca0a2414956f03e39aee4af122d45ac76f620
Maytton <maytton0000@gmail.com>
2025-01-10 10:04:31 -0300

    post repository

--- src/main/java/com/dev/blog/repositories/PostRepository.java
+++ src/main/java/com/dev/blog/repositories/PostRepository.java
@@ -1,4 +1,4 @@
-package com.dev.blog.domain.repositories;
+package com.dev.blog.repositories;
 
 import com.dev.blog.domain.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
