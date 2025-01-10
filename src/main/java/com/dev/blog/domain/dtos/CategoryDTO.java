63d740c59e52421770256dc33868f55a0ed11cd1
Maytton <maytton0000@gmail.com>
2025-01-10 10:04:19 -0300

    category dto

---
+++ src/main/java/com/dev/blog/domain/dtos/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.dev.blog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private UUID id;
+    private String name;
+    private long postCount;
+}
