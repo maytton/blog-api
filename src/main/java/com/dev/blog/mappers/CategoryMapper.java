174893a85e511ba24882982f00ed0f7e878b3270
Maytton <maytton0000@gmail.com>
2025-01-10 10:35:03 -0300

    category mapper

---
+++ src/main/java/com/dev/blog/mappers/CategoryMapper.java
@@ -0,0 +1,30 @@
+package com.dev.blog.mappers;
+
+import com.dev.blog.domain.PostStatus;
+import com.dev.blog.domain.dtos.CategoryDTO;
+import com.dev.blog.domain.entities.Category;
+import com.dev.blog.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDTO toDTO(Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts) {
+        if (null == posts) {
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+
+}
