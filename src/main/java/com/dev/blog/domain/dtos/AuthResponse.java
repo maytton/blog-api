6a0a27c5dbc041abd95d3760bcfbcfcb0b54750d
Maytton <maytton0000@gmail.com>
2025-01-14 08:39:34 -0300

    auth response

---
+++ src/main/java/com/dev/blog/domain/dtos/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.dev.blog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String token;
+    private long expiresIn;
+}
