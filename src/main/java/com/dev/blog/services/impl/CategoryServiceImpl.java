c39fec668098d65963ae289a86c9be24772c8fab
Maytton <maytton0000@gmail.com>
2025-01-16 16:32:46 -0300

    category service impl

--- src/main/java/com/dev/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/dev/blog/services/impl/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.dev.blog.services.impl;
 import com.dev.blog.domain.entities.Category;
 import com.dev.blog.repositories.CategoryRepository;
 import com.dev.blog.services.CategoryService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +20,25 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public Category createCategory(Category category) {
+        if (categoryRepository.existsByNameIgnoreCase(category.getName())) {
+            throw new IllegalStateException("Category with name " + category.getName() + " already exists");
+        }
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(UUID id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent()) {
+            if (!category.get().getPosts().isEmpty()) {
+                throw new IllegalStateException("Category has posts associated with it");
+            }
+            categoryRepository.deleteById(id);
+        }
     }
 }
