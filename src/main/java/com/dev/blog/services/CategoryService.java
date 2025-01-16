f5a8e633a5b222f89dad5a0e870339d1c5b008be
Maytton <maytton0000@gmail.com>
2025-01-16 16:32:18 -0300

    category service

--- src/main/java/com/dev/blog/services/CategoryService.java
+++ src/main/java/com/dev/blog/services/CategoryService.java
@@ -4,8 +4,12 @@ package com.dev.blog.services;
 import com.dev.blog.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
 
     List<Category> listCategories();
+    Category createCategory(Category category);
+
+    void deleteCategory(UUID id);
 }
