0fa7d01144ade8fb1fac3dca858dfaa9541717f2
Maytton <maytton0000@gmail.com>
2025-01-17 17:30:07 -0300

    add metodo para validar token

--- src/main/java/com/dev/blog/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/blog/services/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dev.blog.services.impl;
 
 import com.dev.blog.services.AuthenticationService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -46,6 +47,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpiryMs))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
     }
 
     private Key getSigningKey() {
