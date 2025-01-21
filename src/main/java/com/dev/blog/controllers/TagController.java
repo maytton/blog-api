f1e6b9fad752f72db598623d1a9146e7709de17d
Maytton <maytton0000@gmail.com>
2025-01-21 15:34:11 -0300

    rota de listagem

---
+++ src/main/java/com/dev/blog/controllers/TagController.java
@@ -0,0 +1,29 @@
+package com.dev.blog.controllers;
+
+import com.dev.blog.domain.dtos.TagResponse;
+import com.dev.blog.domain.entities.Tag;
+import com.dev.blog.mappers.TagMapper;
+import com.dev.blog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity<List<TagResponse>> getAll() {
+        List<Tag> tags = tagService.getTags();
+        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
+        return ResponseEntity.ok(tagResponses);
+    }
+}
