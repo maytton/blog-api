11850ca9be316c95f621cb9477183937b72ffd8b
Maytton <maytton0000@gmail.com>
2025-01-21 15:13:38 -0300

    tag mapper

---
+++ src/main/java/com/dev/blog/mappers/TagMapper.java
@@ -0,0 +1,28 @@
+package com.dev.blog.mappers;
+
+import com.dev.blog.domain.PostStatus;
+import com.dev.blog.domain.dtos.TagResponse;
+import com.dev.blog.domain.entities.Post;
+import com.dev.blog.domain.entities.Tag;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TagMapper {
+
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    TagResponse toTagResponse(Tag tag);
+
+    @Named("calculatePostCount")
+    default Integer calculatePostCount(List<Post> posts) {
+        if (posts == null) {
+            return 0;
+        }
+        return (int) posts.stream().filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
