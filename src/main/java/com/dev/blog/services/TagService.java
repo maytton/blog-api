1bec8f1608350ea9bd7916173029cab13d201f16
Maytton <maytton0000@gmail.com>
2025-01-22 08:51:51 -0300

    adicionado metodo que cria tag

--- src/main/java/com/dev/blog/services/TagService.java
+++ src/main/java/com/dev/blog/services/TagService.java
@@ -3,8 +3,10 @@ package com.dev.blog.services;
 import com.dev.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
 
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
