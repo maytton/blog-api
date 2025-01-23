b959494b3acad13794a1adf3bd5e3b2db3c370fa
Maytton <maytton0000@gmail.com>
2025-01-23 17:42:11 -0300

    add metodo para buscar categoria por id

--- src/main/java/com/dev/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/dev/blog/services/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.dev.blog.services.impl;
 import com.dev.blog.domain.entities.Category;
 import com.dev.blog.repositories.CategoryRepository;
 import com.dev.blog.services.CategoryService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -40,5 +41,12 @@ public class CategoryServiceImpl implements CategoryService {
             }
             categoryRepository.deleteById(id);
         }
+    }
+
+    @Override
+    public Category getCategoryById(UUID id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found with id " + id));
+
     }
 }
