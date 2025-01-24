313d1c1ea3cffb172330300f3aeecd7d89712c9e
Maytton <maytton0000@gmail.com>
2025-01-24 17:37:47 -0300

    service para buscar posts

---
+++ src/main/java/com/dev/blog/services/PostService.java
@@ -0,0 +1,11 @@
+package com.dev.blog.services;
+
+import com.dev.blog.domain.entities.Post;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PostService {
+
+    List<Post> getAllPosts(UUID categoryId, UUID tagId);
+}
---
+++ src/main/java/com/dev/blog/services/impl/PostServiceImpl.java
@@ -0,0 +1,57 @@
+package com.dev.blog.services.impl;
+
+import com.dev.blog.domain.PostStatus;
+import com.dev.blog.domain.entities.Category;
+import com.dev.blog.domain.entities.Post;
+import com.dev.blog.domain.entities.Tag;
+import com.dev.blog.repositories.PostRepository;
+import com.dev.blog.services.CategoryService;
+import com.dev.blog.services.PostService;
+import com.dev.blog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final CategoryService categoryService;
+    private final TagService tagService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Post> getAllPosts(UUID categoryId, UUID tagId) {
+        if (categoryId != null && tagId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            Tag tag = tagService.getTagById(tagId);
+            return postRepository.findAllByStatusAndCategoryAndTagsContaining(
+                    PostStatus.PUBLISHED,
+                    category,
+                    tag
+            );
+        }
+
+        if (categoryId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            return postRepository.findAllByStatusAndCategory(
+                    PostStatus.PUBLISHED,
+                    category
+            );
+        }
+
+        if (tagId != null) {
+            Tag tag = tagService.getTagById(tagId);
+            return postRepository.findAllByStatusAndTagsContaining(
+                    PostStatus.PUBLISHED,
+                    tag
+            );
+        }
+
+        return postRepository.findAllByStatus(PostStatus.PUBLISHED);
+    }
+}
