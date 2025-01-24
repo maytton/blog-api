5e03e2c4e19a5d9f18104e57026070770f6a29b9
Maytton <maytton0000@gmail.com>
2025-01-24 17:38:57 -0300

    mudança no nome da classse Tag

--- src/main/java/com/dev/blog/controllers/TagController.java
+++ src/main/java/com/dev/blog/controllers/TagController.java
@@ -1,7 +1,7 @@
 package com.dev.blog.controllers;
 
 import com.dev.blog.domain.dtos.CreateTagRequest;
-import com.dev.blog.domain.dtos.TagResponse;
+import com.dev.blog.domain.dtos.TagDTO;
 import com.dev.blog.domain.entities.Tag;
 import com.dev.blog.mappers.TagMapper;
 import com.dev.blog.services.TagService;
@@ -22,18 +22,18 @@ public class TagController {
     private final TagMapper tagMapper;
 
     @GetMapping
-    public ResponseEntity<List<TagResponse>> getAll() {
+    public ResponseEntity<List<TagDTO>> getAll() {
         List<Tag> tags = tagService.getTags();
-        List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
-        return ResponseEntity.ok(tagResponses);
+        List<TagDTO> tagRespons = tags.stream().map(tagMapper::toTagResponse).toList();
+        return ResponseEntity.ok(tagRespons);
     }
 
     @PostMapping
-    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagRequest createTagRequest) {
+    public ResponseEntity<List<TagDTO>> createTags(@RequestBody CreateTagRequest createTagRequest) {
         List<Tag> savedTags = tagService.createTags(createTagRequest.getNames());
-        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        List<TagDTO> createdTagRespons = savedTags.stream().map(tagMapper::toTagResponse).toList();
         return new ResponseEntity<>(
-                createdTagResponses,
+                createdTagRespons,
                 HttpStatus.CREATED
         );
     }
