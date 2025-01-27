30c16282366cc7cd6c40cc8aedd58de01a59da19
Maytton <maytton0000@gmail.com>
2025-01-27 09:58:30 -0300

    metodo para buscar drafts

--- src/main/java/com/dev/blog/services/impl/PostServiceImpl.java
+++ src/main/java/com/dev/blog/services/impl/PostServiceImpl.java
@@ -4,6 +4,7 @@ import com.dev.blog.domain.PostStatus;
 import com.dev.blog.domain.entities.Category;
 import com.dev.blog.domain.entities.Post;
 import com.dev.blog.domain.entities.Tag;
+import com.dev.blog.domain.entities.User;
 import com.dev.blog.repositories.PostRepository;
 import com.dev.blog.services.CategoryService;
 import com.dev.blog.services.PostService;
@@ -53,5 +54,10 @@ public class PostServiceImpl implements PostService {
         }
 
         return postRepository.findAllByStatus(PostStatus.PUBLISHED);
+    }
+
+    @Override
+    public List<Post> getDrafts(User user) {
+        return postRepository.findAllByAuthorAndStatus(user, PostStatus.DRAFT);
     }
 }
