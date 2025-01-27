a9371788aadb1ec7dfc2968a45e4d49dfbe595b2
Maytton <maytton0000@gmail.com>
2025-01-27 09:58:09 -0300

    metodo para buscar posts por autor e status

--- src/main/java/com/dev/blog/repositories/PostRepository.java
+++ src/main/java/com/dev/blog/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import com.dev.blog.domain.PostStatus;
 import com.dev.blog.domain.entities.Category;
 import com.dev.blog.domain.entities.Post;
 import com.dev.blog.domain.entities.Tag;
+import com.dev.blog.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +17,5 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
     List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tag);
     List<Post> findAllByStatus(PostStatus status);
+    List<Post> findAllByAuthorAndStatus(User author, PostStatus status);
 }
