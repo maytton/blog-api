02d93a0c9238871ded0d52db463f18f51b9812e6
Maytton <maytton0000@gmail.com>
2025-01-27 09:58:25 -0300

    metodo para buscar drafts

--- src/main/java/com/dev/blog/services/PostService.java
+++ src/main/java/com/dev/blog/services/PostService.java
@@ -1,6 +1,7 @@
 package com.dev.blog.services;
 
 import com.dev.blog.domain.entities.Post;
+import com.dev.blog.domain.entities.User;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,4 +9,5 @@ import java.util.UUID;
 public interface PostService {
 
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
+    List<Post> getDrafts(User user);
 }
