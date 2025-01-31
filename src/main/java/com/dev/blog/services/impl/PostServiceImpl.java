45724972adc2cb212df8f7ad5bdf3a679e33ed96
Maytton <maytton0000@gmail.com>
2025-01-31 17:26:03 -0300

    metodo para criar um post

--- src/main/java/com/dev/blog/services/impl/PostServiceImpl.java
+++ src/main/java/com/dev/blog/services/impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dev.blog.services.impl;
 
+import com.dev.blog.domain.CreatePostRequest;
 import com.dev.blog.domain.PostStatus;
 import com.dev.blog.domain.entities.Category;
 import com.dev.blog.domain.entities.Post;
@@ -13,7 +14,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -23,6 +26,8 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final CategoryService categoryService;
     private final TagService tagService;
+
+    private static final int WORDS_PER_MINUTE = 200;
 
     @Override
     @Transactional(readOnly = true)
@@ -59,5 +64,34 @@ public class PostServiceImpl implements PostService {
     @Override
     public List<Post> getDrafts(User user) {
         return postRepository.findAllByAuthorAndStatus(user, PostStatus.DRAFT);
+    }
+
+    @Override
+    @Transactional
+    public Post createPost(User user, CreatePostRequest createPostRequest) {
+        Post newPost = new Post();
+        newPost.setTitle(createPostRequest.getTitle());
+        newPost.setContent(createPostRequest.getContent());
+        newPost.setStatus(createPostRequest.getStatus());
+        newPost.setAuthor(user);
+        newPost.setReadingTime(calculateReadingTime(createPostRequest.getContent()));
+
+        Category category = categoryService.getCategoryById(createPostRequest.getCategoryId());
+        newPost.setCategory(category);
+
+        Set<UUID> tagIds = createPostRequest.getTagsId();
+        List<Tag> tags = tagService.getTagsByIds(tagIds);
+        newPost.setTags(new HashSet<>(tags));
+
+        return postRepository.save(newPost);
+
+    }
+
+    private Integer calculateReadingTime(String content) {
+        if (content == null || content.isEmpty()) {
+            return 0;
+        }
+        int wordCount = content.trim().split("\\s+").length;
+        return (int) Math.ceil((double) wordCount / WORDS_PER_MINUTE);
     }
 }
