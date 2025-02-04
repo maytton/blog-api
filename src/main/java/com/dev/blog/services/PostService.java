4cd58a544ef9fa8a078d27c349f0c9d843213c7c
Maytton <maytton0000@gmail.com>
2025-02-04 16:45:56 -0300

    add update post request

--- src/main/java/com/dev/blog/services/PostService.java
+++ src/main/java/com/dev/blog/services/PostService.java
@@ -1,6 +1,7 @@
 package com.dev.blog.services;
 
 import com.dev.blog.domain.CreatePostRequest;
+import com.dev.blog.domain.UpdatePostRequest;
 import com.dev.blog.domain.entities.Post;
 import com.dev.blog.domain.entities.User;
 
@@ -12,4 +13,5 @@ public interface PostService {
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
     List<Post> getDrafts(User user);
     Post createPost(User user, CreatePostRequest createPostRequest);
+    Post updatePost(UUID id, UpdatePostRequest updatePostRequest);
 }
